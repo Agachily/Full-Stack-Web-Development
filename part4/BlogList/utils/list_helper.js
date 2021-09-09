76ed71248640267a4ab7bc084295379b530ae03c
Agachily <zetongzhao@foxmail.com>
2021-09-09 20:33:45 +0300

    finish exercise 4.5

--- part4/BlogList/utils/list_helper.js
+++ part4/BlogList/utils/list_helper.js
@@ -6,11 +6,28 @@ const likeSum = (array) => {
     let sum = 0
     array.forEach(element => {
         sum = sum + element.likes
-    });
+    })
     return sum
+}
+
+const favoriteBlog = (array) => {
+    let max = 0
+    let maxIndex = 0
+    array.forEach((element,index) => {
+        if (element.likes > max){
+            max = element.likes
+            maxIndex = index
+        }
+    })
+    return({
+      title: array[maxIndex].title,
+      author: array[maxIndex].author,
+      likes: array[maxIndex].likes
+    })
 }
 
 module.exports = {
     dummy,
-    likeSum
+    likeSum,
+    favoriteBlog
 }
