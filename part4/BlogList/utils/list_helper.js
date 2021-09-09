67619013f1bc771c5e9f57ad7eb1b636099d378c
Agachily <zetongzhao@foxmail.com>
2021-09-09 20:17:29 +0300

    finish exercise 4.4

--- part4/BlogList/utils/list_helper.js
+++ part4/BlogList/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const likeSum = (array) => {
+    let sum = 0
+    array.forEach(element => {
+        sum = sum + element.likes
+    });
+    return sum
+}
+
 module.exports = {
-    dummy
+    dummy,
+    likeSum
 }
